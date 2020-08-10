ae3eae38dae71733db3a5bcfe578ebbfde796a07
freediaminds-9992 <69465002+freediaminds-9992@users.noreply.github.com>
2020-08-10 18:17:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyA2ADvUVfvgvKUUEg6eV2bK56vMcgOj8FI",
+  authDomain: "ff-hacking.firebaseapp.com",
+  databaseURL: "https://ff-hacking.firebaseio.com",
+  projectId: "ff-hacking",
+  storageBucket: "ff-hacking.appspot.com",
+  messagingSenderId: "100072755196",
+  appId: "1:100072755196:web:bd048b5b37ffa4ebc16c79",
+  measurementId: "G-0G7RS48GRB"
 };
 
      
